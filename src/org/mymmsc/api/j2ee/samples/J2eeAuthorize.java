d4c7e7adb90948dcec94c557dcc2d32aca7bef34
王锋 <wangfeng@yeah.net>
2014-09-05 13:55:05 +0800

    完整的测试代码

---
+++ src/org/mymmsc/api/j2ee/samples/J2eeAuthorize.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.mymmsc.api.j2ee.samples;
+
+import org.mymmsc.api.assembly.Api;
+
+/**
+ * @author wangfeng
+ *
+ */
+public class J2eeAuthorize {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String project = "wifi-api";
+		String macAddr = "";
+		String userId = "SH0001";
+		String expire = "2014-01-31 00:00:00";
+		
+		String tmpKey = Api.md5(userId + "mymmsc" + expire + "j2ee" + project);
+		System.out.println("key=[" + tmpKey + "]");
+	}
+
+}
