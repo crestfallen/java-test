9e98429ff615ab6af5adb55d925985ea1b48119c
王锋 <wangfeng@jiedaibao.com>
2016-07-30 09:15:45 +0800

    输出json串是忽略静态成员变量

--- src/main/java/org/mymmsc/aio2/test/AbstractMinHeap.java
+++ src/main/java/org/mymmsc/aio2/test/AbstractMinHeap.java
@@ -1,27 +1,27 @@
 /**
- * @(#)AbstractMinHeap.java	6.3.9 09/11/02
- *
+ * @(#)AbstractMinHeap.java 6.3.9 09/11/02
+ * <p>
  * Copyright 2000-2010 MyMMSC Software Foundation (MSF), Inc. All rights reserved.
  * MyMMSC PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 package org.mymmsc.aio2.test;
 
 /**
+ * @param <T>
  * @author WangFeng(wangfeng@yeah.net)
  * @version 6.3.9 09/10/02
  * @since mymmsc-api 6.3.9
- * @param <T>
  */
 public abstract class AbstractMinHeap<T> {
 
-	private int m_index = -1;
+    private int m_index = -1;
 
-	public void setIndex(int index) {
-		this.m_index = index;
-	}
+    public int getIndex() {
+        return m_index;
+    }
 
-	public int getIndex() {
-		return m_index;
-	}
+    public void setIndex(int index) {
+        this.m_index = index;
+    }
 
 }
