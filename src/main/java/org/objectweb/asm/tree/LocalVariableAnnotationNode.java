4c1fa5cfac261316cd3f17879818d412c7b98b09
王锋 <wangfeng@jiedaibao.com>
2016-07-30 10:53:53 +0800

    输出json串是忽略静态成员变量

--- src/main/java/org/objectweb/asm/tree/LocalVariableAnnotationNode.java
+++ src/main/java/org/objectweb/asm/tree/LocalVariableAnnotationNode.java
@@ -135,5 +135,4 @@ public class LocalVariableAnnotationNode extends TypeAnnotationNode {
         accept(mv.visitLocalVariableAnnotation(typeRef, typePath, start, end,
                 index, desc, true));
     }
-
 }
