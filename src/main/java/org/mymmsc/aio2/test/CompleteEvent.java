9b54ffde028eaa0b07556daf856ebb8a75dd8cc8
王锋 <wangfeng@jiedaibao.com>
2016-07-30 09:20:47 +0800

    输出json串是忽略静态成员变量

--- src/main/java/org/mymmsc/aio2/test/CompleteEvent.java
+++ src/main/java/org/mymmsc/aio2/test/CompleteEvent.java
@@ -9,35 +9,35 @@ package org.mymmsc.aio2.test;
 import java.nio.channels.SelectionKey;
 
 public abstract class CompleteEvent extends SelectionKey {
-	public final static int IOCP_UNKNOWN = 0x0000;
-	public final static int IOCP_ACCEPT = 0x0001;
-	public final static int IOCP_CONNECT = 0x0002;
-	public final static int IOCP_CLOSED = 0x0004;
-	public final static int IOCP_READ = 0x0008;
-	public final static int IOCP_TIMEOUT = 0x0010;
-	public final static int IOCP_WRITE = 0x0020;
-	private int event = IOCP_UNKNOWN;
+    public final static int IOCP_UNKNOWN = 0x0000;
+    public final static int IOCP_ACCEPT = 0x0001;
+    public final static int IOCP_CONNECT = 0x0002;
+    public final static int IOCP_CLOSED = 0x0004;
+    public final static int IOCP_READ = 0x0008;
+    public final static int IOCP_TIMEOUT = 0x0010;
+    public final static int IOCP_WRITE = 0x0020;
+    private int event = IOCP_UNKNOWN;
 
-	public CompleteEvent(SelectionKey key) {
-		event = IOCP_UNKNOWN;
-		if (key == null) {
+    public CompleteEvent(SelectionKey key) {
+        event = IOCP_UNKNOWN;
+        if (key == null) {
 
-		} else if (key.isAcceptable()) {
-			event = IOCP_ACCEPT;
-		} else if (key.isConnectable()) {
-			event = IOCP_CONNECT;
-		} else if (key.isReadable()) {
-			event = IOCP_READ;
-		} else if (key.isWritable()) {
-			event = IOCP_WRITE;
-		} else if (key.isValid()) {
-			//
-		} else {
-			//
-		}
-	}
+        } else if (key.isAcceptable()) {
+            event = IOCP_ACCEPT;
+        } else if (key.isConnectable()) {
+            event = IOCP_CONNECT;
+        } else if (key.isReadable()) {
+            event = IOCP_READ;
+        } else if (key.isWritable()) {
+            event = IOCP_WRITE;
+        } else if (key.isValid()) {
+            //
+        } else {
+            //
+        }
+    }
 
-	public int getEvent() {
-		return event;
-	}
+    public int getEvent() {
+        return event;
+    }
 }
